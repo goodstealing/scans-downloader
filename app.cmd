4aac5992bb0d7f27eb8f367cfcc24f2b50c55118
goodstealing <xxxvlados@gmail.com>
2023-08-27 17:14:24 +0300

    app

---
+++ app.cmd
@@ -0,0 +1,6 @@
+
+@echo off
+rem This script was created by Nuitka to execute 'app.exe' with Python DLL being found.
+set PATH=c:\users\GOODST~1\appdata\local\programs\python\PYTHON~1;%PATH%
+set PYTHONHOME=C:\Users\goodstealing\AppData\Local\Programs\Python\Python310
+"%~dp0.\app.exe" %*
